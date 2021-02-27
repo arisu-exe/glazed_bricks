6edd54ead25986e5b346506703635b7c06da137b
fallOut015 <captainDoodles264@gmail.com>
2021-02-26 23:37:25 -0800

    blockstate gen

--- src/main/java/io/github/fallOut015/glazed_bricks/data/DataGeneratorGlazedBricks.java
+++ src/main/java/io/github/fallOut015/glazed_bricks/data/DataGeneratorGlazedBricks.java
@@ -1,27 +1,10 @@
 package io.github.fallOut015.glazed_bricks.data;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import io.github.fallOut015.glazed_bricks.MainGlazedBricks;
-import io.github.fallOut015.glazed_bricks.block.BlocksGlazedBricks;
-import io.github.fallOut015.glazed_bricks.data.server.LootTableProviderGlazedBricks;
-import net.minecraft.block.Block;
-import net.minecraft.block.FlowerPotBlock;
-import net.minecraft.block.GlassBlock;
+import io.github.fallOut015.glazed_bricks.data.client.BlockStateProviderGlazedBricks;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DirectoryCache;
-import net.minecraft.data.IDataProvider;
-import net.minecraft.data.Main;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
 
 public class DataGeneratorGlazedBricks {
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -32,39 +15,13 @@ public class DataGeneratorGlazedBricks {
             DataGenerator gen = event.getGenerator();
 
             if(event.includeClient()) {
-                final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-                Path outputFolder = gen.getOutputFolder();
-
-                Block block = BlocksGlazedBricks.WHITE_GLAZED_FLOWER_POT.get();
-                ResourceLocation key = block.getRegistryName();
-                Path path = outputFolder.resolve("assets/" + key.getNamespace() + "/blockstates/" + key.getPath() + ".json");
-
-                JsonObject blockState = new JsonObject();
-                JsonObject variants = new JsonObject();
-                JsonObject variant = new JsonObject();
-                variant.addProperty("model", key.getNamespace() + ":block/" + key.getPath());
-                variants.add("", variant);
-                blockState.add("variants", variants);
-
-                //File f = new File(path.toString() + "");
-
-                try {
-                    DirectoryCache cache = new DirectoryCache(outputFolder, "cache");
-                    IDataProvider.save(GSON, cache, blockState, path);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                for(RegistryObject<Block> block2 : BlocksGlazedBricks.getEntries()) {
-                    if(block2.get() instanceof FlowerPotBlock) {
-
-                    }
-                }
+                //gen.addProvider(new BlockStateProviderGlazedBricks(gen));
+                //gen.addProvider(new BlockModelProviderGlazedBricks(gen, event.getExistingFileHelper()));
             }
 
             if(event.includeServer()) {
-				gen.addProvider(new LootTableProviderGlazedBricks(gen));
-                //gen.addProvider(new TagProviderGlazedBricks(gen, Registry.BLOCK, MainGlazedBricks.MODID, event.getExistingFileHelper()));
+				//gen.addProvider(new LootTableProviderGlazedBricks(gen));
+                //gen.addProvider(new TagProviderGlazedBricks(gen, event.getExistingFileHelper()));
             }
         }
     }
---
+++ src/main/java/io/github/fallOut015/glazed_bricks/data/client/BlockStateProviderGlazedBricks.java
@@ -0,0 +1,66 @@
+package io.github.fallOut015.glazed_bricks.data.client;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import io.github.fallOut015.glazed_bricks.block.BlocksGlazedBricks;
+import net.minecraft.block.Block;
+import net.minecraft.block.FlowerPotBlock;
+import net.minecraft.data.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraftforge.fml.RegistryObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class BlockStateProviderGlazedBricks extends BlockStateProvider {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().disableHtmlEscaping().create();
+    private final DataGenerator gen;
+
+    public BlockStateProviderGlazedBricks(DataGenerator gen) {
+        super(gen);
+        this.gen = gen;
+    }
+
+    public void act(DirectoryCache cache) {
+        Path path = this.gen.getOutputFolder();
+        Map<Block, IFinishedBlockState> map = Maps.newHashMap();
+
+        List<RegistryObject<Block>> list = BlocksGlazedBricks.getEntries().stream().filter(block -> !map.containsKey(block.get())).collect(Collectors.toList());
+
+        list.forEach(block -> {
+            if(block.get() instanceof FlowerPotBlock) {
+                BlockModelDefinition model = new BlockModelDefinition().replaceInfoValue(BlockModelFields.field_240202_c_, new ResourceLocation(block.get().getRegistryName().getNamespace() + ":block/" + block.get().getRegistryName().getPath()));
+                map.put(block.get(), FinishedVariantBlockState.func_240120_a_(block.get(), model));
+            }
+        });
+
+        this.write(cache, path, map, BlockStateProviderGlazedBricks::blockStateResolver);
+    }
+
+    private <T> void write(DirectoryCache cache, Path path1, Map<T, ? extends Supplier<JsonElement>> files, BiFunction<Path, T, Path> pathGetter) {
+        files.forEach((key, file) -> {
+            Path path = pathGetter.apply(path1, key);
+
+            try {
+                IDataProvider.save(GSON, cache, file.get(), path);
+            } catch (Exception exception) {
+                LOGGER.error("Couldn't save {}", path, exception);
+            }
+        });
+    }
+
+    private static Path blockStateResolver(Path p_240082_0_, Block p_240082_1_) {
+        ResourceLocation resourcelocation = Registry.BLOCK.getKey(p_240082_1_);
+        return p_240082_0_.resolve("assets/" + resourcelocation.getNamespace() + "/blockstates/" + resourcelocation.getPath() + ".json");
+    }
+}
